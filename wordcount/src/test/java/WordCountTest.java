4adb6bd8afa1157b1f9e1f1a1af338a6ebd9ae7e
Jeffrey M Lutz <jeff.lutz@agileanswers.com>
2012-11-26 17:10:09 -0500

    Added mrunit example (wordcount)

---
+++ wordcount/src/test/java/WordCountTest.java
@@ -0,0 +1,61 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User: Jeffrey M Lutz
+ * Date: 11/26/12
+ * Time: 1:19 PM
+ */
+public class WordCountTest {
+
+
+    MapReduceDriver<LongWritable, Text, Text, IntWritable, Text, IntWritable> mapReduceDriver;
+    MapDriver mapDriver;
+    ReduceDriver<Text, IntWritable, Text, IntWritable> reduceDriver;
+
+    @Before
+    public void before() {
+        WordMapper mapper = new WordMapper();
+        SumReducer reducer = new SumReducer();
+
+        mapDriver = MapDriver.newMapDriver(mapper);
+        reduceDriver = ReduceDriver.newReduceDriver(reducer);
+        mapReduceDriver = MapReduceDriver.newMapReduceDriver(mapper, reducer);
+    }
+
+    @Test
+    public void testMapper() {
+        mapDriver.withInput(new LongWritable(1), new Text("cat cat dog"));
+        mapDriver.withOutput(new Text("cat"), new IntWritable(1));
+        mapDriver.withOutput(new Text("cat"), new IntWritable(1));
+        mapDriver.withOutput(new Text("dog"), new IntWritable(1));
+        mapDriver.runTest();
+    }
+
+    @Test
+    public void testReducer() {
+        List<IntWritable> values = new ArrayList<IntWritable>();
+        values.add(new IntWritable(1));
+        values.add(new IntWritable(1));
+        reduceDriver.withInput(new Text("cat"), values);
+        reduceDriver.withOutput(new Text("cat"), new IntWritable(2));
+        reduceDriver.runTest();
+    }
+
+    @Test
+    public void testMapReduce() {
+        mapReduceDriver.withInput(new LongWritable(1), new Text("cat cat dog"));
+        mapReduceDriver.addOutput(new Text("cat"), new IntWritable(2));
+        mapReduceDriver.addOutput(new Text("dog"), new IntWritable(1));
+        mapReduceDriver.runTest();
+    }
+}
